d628a496b4c4acdd143eb193b46e511712ebb35b
ran <ran2rrrw@gmail.com>
2023-11-02 17:37:21 +0900

    ~ Step09

---
+++ Step09_AbstractClass/src/test/mypac/MyWeapon.java
@@ -0,0 +1,12 @@
+package test.mypac;
+
+public class MyWeapon extends Weapon {
+
+	@Override
+	public void attack() {
+		//어디를 공격할지는 프로젝트 상황에 맞게 마음대로 공격하면 된다.
+		System.out.println("지상 공격을 해요!");
+		
+	}
+
+}
